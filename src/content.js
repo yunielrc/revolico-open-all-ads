ddd53ba5e7b7de5082f509515e7ade57a863e190
yuniel <yunielrc87@gmail.com>
2020-11-24 17:32:15 -0500

    remove debug log

--- src/content.js
+++ src/content.js
@@ -1,7 +1,6 @@
 chrome.runtime.onMessage.addListener(
   (request, sender, sendResponse) => {
     if (request.method === 'get-ads-urls') {
-      console.log(request)
       const urls = []
       document.querySelectorAll('ul > li[data-cy="adRow"] > a[href]').forEach(e => { urls.push(e.href) })
       sendResponse({ result: urls })
